535c39896a89db42f725f9597e3852d0ef086595
nryeouo <43399370+nryeouo@users.noreply.github.com>
2022-07-29 16:54:26 +0900

    search posts

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, g, Markup
+from flask_paginate import Pagination, get_page_parameter
 
 import sqlite3
 import datetime
@@ -51,3 +52,24 @@ def view_one_article(article_id):
     res = cur.fetchone()
     cur.close()
     return render_template("detail.html", res=res, title="投稿詳細")
+
+
+@app.route("/search")
+def view_search_results():
+    query = request.args.get("q")
+    if len(query) > 0:
+        conn = connect_db()
+        cur = conn.execute("select article_title, author_name, author_remote_addr, \
+         strftime('%Y-%m-%d %H:%M:%S', created_at) as created_at, article_text, article_id \
+          from articles where article_text like ?", ("%"+query+"%",))
+        res = cur.fetchall()
+        cur.close()
+        page = request.args.get(get_page_parameter(), type=int, default=1)
+        res_p = res[(page-1)*25: page*25]
+        page_disp_msg = "{total}件の結果。{start}から{end}件目を表示中"
+        pagination = Pagination(page=page, total=len(res), per_page=25,
+                                css_framework="bootstrap5", display_msg=page_disp_msg)
+        return render_template("index_paginate.html", rows=res_p, pagination=pagination,
+                               title=query, total=len(res))
+    else:
+        return render_template("index.html", title="トップ")
